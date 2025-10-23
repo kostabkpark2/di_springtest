45f835a1ceee1ad323c53bfe77f22d7ecc624685
kostabkpark2 <kostabkpark2@gmail.com>
2025-10-23 16:12:24 +0900

    mybatis dependency add

--- src/test/java/org/example/di_springtest/repository/PostRepositoryImplTest.java
+++ src/test/java/org/example/di_springtest/repository/PostRepositoryImplTest.java
@@ -1,12 +1,20 @@
 package org.example.di_springtest.repository;
 
+import org.example.di_springtest.model.Post;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
 class PostRepositoryImplTest {
+  @Autowired
+  private PostRepositoryImpl postRepository;
 
   @BeforeEach
   void setUp() {
@@ -18,14 +26,28 @@ class PostRepositoryImplTest {
 
   @Test
   void findAll() {
-  }
-
-  @Test
-  void insertPost() {
+    // given
+    Post post1 = new Post();
+    post1.setTitle("test1");
+    post1.setBody("bodytest1");
+    postRepository.insertPost(post1);
+    // when
+    List<Post> all = postRepository.findAll();
+    // then
+    assertThat(all.size()).isEqualTo(1);
   }
 
   @Test
   void findById() {
+    // given
+    Post post1 = new Post();
+    post1.setTitle("test1");
+    post1.setBody("bodytest1");
+    int postId = postRepository.insertPost(post1);
+    // when
+    Post findPost = postRepository.findById(postId);
+    // then
+    assertThat(findPost.getTitle()).isEqualTo("test1");
   }
 
   @Test
---
+++ src/test/java/org/example/di_springtest/service/PostDtoServiceTest.java
@@ -0,0 +1,42 @@
+package org.example.di_springtest.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostDtoServiceTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void getAllPost() {
+  }
+
+  @Test
+  void createPost() {
+  }
+
+  @Test
+  void selectPost() {
+  }
+
+  @Test
+  void updatePost() {
+  }
+
+  @Test
+  void deletePost() {
+  }
+
+  @Test
+  void updateLikesPost() {
+  }
+}
