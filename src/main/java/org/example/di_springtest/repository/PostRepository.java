9658be8a2ff26351395f3ebd3eb50f5a182fc79d
kostabkpark2 <kostabkpark2@gmail.com>
2025-10-23 16:44:16 +0900

    mybatis xml method add

--- src/main/java/org/example/di_springtest/repository/PostRepository.java
+++ src/main/java/org/example/di_springtest/repository/PostRepository.java
@@ -1,10 +1,12 @@
 package org.example.di_springtest.repository;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.example.di_springtest.model.Post;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Mapper
 public interface PostRepository {
   public List<Post> findAll();
   public int insertPost(Post post);
--- src/main/java/org/example/di_springtest/repository/PostRepositoryImpl.java
+++ src/main/java/org/example/di_springtest/repository/PostRepositoryImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
+//@Repository
 public class PostRepositoryImpl implements PostRepository {
   private final Map<Integer, Post> posts;
   private int sequence ;
